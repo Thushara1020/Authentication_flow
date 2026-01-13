debee17376eaf8532792adb9b7cc7060c7decb26
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-13 10:38:22 +0530

    add LoginForm

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+
+    @FXML
+    private Button btnSignIn;
+
+    @FXML
+    private Hyperlink btnSignUp;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void SignUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SingInOnAction(ActionEvent event) {
+
+    }
+
 }
